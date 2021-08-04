5928cfc155d590bc5df0b2cfa1de32901eeae9d0
Aschius <aschius@protonmail.com>
2021-08-04 18:19:53 +0200

    Setting it back to the way it was before

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 require('dotenv').config({ path: `.env.${process.env.ENV}` });
 
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// });
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const webpack = require('webpack');
 
-module.exports = {
-  //withBundleAnalyzer({
+module.exports = withBundleAnalyzer({
   webpack: (config) => {
     config.plugins.push(new webpack.EnvironmentPlugin(process.env));
     return config;
   },
-};
+});
