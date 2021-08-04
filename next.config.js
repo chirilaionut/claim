32a9defa71d403e51bdd3e53cd17006eb82e7e66
Aschius <aschius@protonmail.com>
2021-08-04 17:47:22 +0200

    Nothing

--- next.config.js
+++ next.config.js
@@ -1,32 +1,15 @@
-// require('dotenv').config({ path: `.env.${process.env.ENV}` });
+require('dotenv').config({ path: `.env.${process.env.ENV}` });
 
-// const { parsed: myEnv } = require('dotenv').config({
-//   path: `.env.${process.env.ENV}`,
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
 // });
-
-const Dotenv = require('dotenv-webpack');
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
 
 const webpack = require('webpack');
 
-module.exports = withBundleAnalyzer({
+module.exports = {
+  //withBundleAnalyzer({
   webpack: (config) => {
-    config.plugins = config.plugins || [];
-
-    config.plugins = [
-      ...config.plugins,
-
-      // Read the .env file
-      new Dotenv({
-        path: `.env.${process.env.ENV}`, //path.join(__dirname, '.env'),
-        systemvars: true,
-      }),
-    ];
-
-    // config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
     return config;
   },
-});
+};
